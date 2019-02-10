2400b3ad39957277d44b8f6f0dc968f04e1dbaa2
36044910 <36044910@qq.com>
2019-02-10 18:51:56 +0800

    修改提交

---
+++ src/main/java/com/indigo/filemanager/controller/OutController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.indigo.filemanager.controller;
+
+import com.indigo.filemanager.service.FileTransferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@RestController
+public class OutController {
+    @Autowired
+    private FileTransferService fileTransferService;
+	@RequestMapping("/files/view/{filekey}")
+	public String viewfile(@PathVariable("filekey") String filekey, HttpServletResponse response) throws IOException {
+		InputStream is = new FileInputStream("E:\\dgy\\projects\\filemanager\\filemanager\\123.pdf");
+		OutputStream ops=response.getOutputStream();
+		response.setContentType("application/pdf");
+		 ops.flush();
+		if (is != null) {
+			byte buff[] = new byte[1048576];
+			for (int byteread = 0; (byteread = is.read(buff)) != -1; ) {
+				ops.write(buff, 0, byteread);
+				ops.flush();
+			}
+
+		}
+		return null;
+	}
+
+}
--- src/test/java/com/indigo/filemanager/FilemanagerApplicationTests.java
+++ src/test/java/com/indigo/filemanager/FilemanagerApplicationTests.java
@@ -1,16 +1,34 @@
 package com.indigo.filemanager;
 
+import com.indigo.filemanager.service.FileTransferService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FilemanagerApplicationTests {
 
+	@Autowired
+	private FileTransferService fileTransferService;
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void convert() throws IOException {
+//		BufferedInputStream bis = new BufferedInputStream(new FileInputStream("E:\\dgy\\projects\\filemanager\\filemanager\\123.doc"));
+//		InputStream in = new FileInputStream(new File("E:\\dgy\\projects\\filemanager\\filemanager\\123.doc"));
+		InputStream inputStream = new FileInputStream("E:\\dgy\\projects\\filemanager\\filemanager\\123.doc");
+		OutputStream outputStream=fileTransferService.transferPdf(inputStream,"doc","pdf");
+		outputStream.close();
 	}
 
 }
