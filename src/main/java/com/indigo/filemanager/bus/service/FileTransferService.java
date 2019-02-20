0f46560fe7548865f67fed467b58547ba9dd6f48
36044910 <36044910@qq.com>
2019-02-20 10:59:01 +0800

    修改转换增加wps

--- src/main/java/com/indigo/filemanager/bus/service/FileTransferService.java
+++ src/main/java/com/indigo/filemanager/bus/service/FileTransferService.java
@@ -1,71 +1,34 @@
 package com.indigo.filemanager.bus.service;
 
-import com.artofsolving.jodconverter.DocumentConverter;
-import com.artofsolving.jodconverter.DocumentFormat;
-import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
-import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
-import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
-import com.indigo.filemanager.bus.service.OwnDocumentFormatRegistry;
+import com.indigo.filemanager.bus.service.util.OpenOffice2PdfUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Service
 public class FileTransferService {
 
-    @Value("${openoffice_home}")
-    private String OpenOffice_HOME;
-    @Value("${openoffice_convert_tempdir}")
-    private String openoffice_convert_tempdir;
-    @Value("${openoffice_startservice}")
+    @Value("${openoffice.home}")
+    private String openOffice_HOME;
+    @Value("${converted.file.type}")
+    private String converted_file_type;
+    @Value("${openoffice.startservice}")
     private String openoffice_startservice;
+    @Value("${saveas.tempdir}")
+    private String saveas_tempdir;
     public OutputStream transferPdf(InputStream inputStream,String filetype,String outFileType){
-        if (OpenOffice_HOME.charAt(OpenOffice_HOME.length() - 1) != '/') {
-            OpenOffice_HOME += "/";
+        OutputStream outputStream=null;
+        switch(converted_file_type){
+            case "1":
+                outputStream= OpenOffice2PdfUtil.transferPdf(inputStream,filetype,outFileType,openOffice_HOME,openoffice_startservice);
+                break;
+            default:
+                //
+                System.out.println("default");
+                break;
         }
-        Process pro = null;
-
-        try {
-
-//            // 如果目标路径不存在, 则新建该路径
-            File outputFile = new File(openoffice_convert_tempdir);
-            if (!outputFile.getParentFile().exists()) {
-                outputFile.getParentFile().mkdirs();
-            }
-//             启动OpenOffice的服务
-            String command = OpenOffice_HOME
-                    + openoffice_startservice;
-            pro = Runtime.getRuntime().exec(command);
-            OpenOfficeConnection connection = new SocketOpenOfficeConnection("127.0.0.1", 8100);
-            connection.connect();
-
-            // convert
-            DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
-            // 2:获取Format
-            OwnDocumentFormatRegistry factory = new OwnDocumentFormatRegistry();
-            DocumentFormat inputDocumentFormat = factory
-                    .getFormatByFileExtension(filetype);
-            DocumentFormat outputDocumentFormat = factory
-                    .getFormatByFileExtension(outFileType);
-            // 3:执行转换
-//            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(openoffice_convert_tempdir+"/"+System.currentTimeMillis()+"."+outFileType));
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            converter.convert(inputStream, inputDocumentFormat,bos,outputDocumentFormat);
-            connection.disconnect();
-            // 封闭OpenOffice服务的进程
-            pro.destroy();
-            return bos;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            pro.destroy();
-        }
-
-        return null;
-
+        return outputStream;
     }
-
-
 }
-
