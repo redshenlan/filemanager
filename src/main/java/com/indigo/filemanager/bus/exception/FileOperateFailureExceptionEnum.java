ddfb2f57bf14dff4587d636625974c789a76736f
yuxiangel007 <yuxiangel007@163.com>
2019-02-20 11:14:59 +0800

    文件操作异常

---
+++ src/main/java/com/indigo/filemanager/bus/exception/FileOperateFailureExceptionEnum.java
@@ -0,0 +1,14 @@
+package com.indigo.filemanager.bus.exception;
+/**
+ * @Description:
+ * @author: qiaoyuxi
+ * @time: 2019年2月20日 上午9:31:35
+ */
+public enum FileOperateFailureExceptionEnum {
+
+	//文件持久化失败
+	FILE_PERSISTENCE_FAIL,
+	
+	//IO出错
+	IO_ERROR;
+}
